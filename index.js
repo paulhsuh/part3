b3ebc5a483c3d2ff5db3d93eb823241870fb3ba0
Paul Suh <phs92@cornell.edu>
2022-07-16 11:10:45 -0700

    3.19

--- index.js
+++ index.js
@@ -4,28 +4,6 @@ const cors = require('cors')
 var morgan = require('morgan')
 const Person = require('./models/person')
 
-let persons = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-]
 
 const app = express()
 app.use(express.json())
@@ -110,7 +88,7 @@ app.put('/api/persons/:id', (request, response, next) => {
     number: body.number
   }
   Person
-    .findByIdAndUpdate(id, newPerson, {new:true})
+    .findByIdAndUpdate(id, newPerson, { new: true, runValidators: true, context: 'query' })
     .then( (result) => response.json(result))
     .catch(error => next(error))
 })
@@ -150,7 +128,10 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  }
+  else if (error.name === 'ValidationError') {
+    return response.status(400).json({error: error.message})
+  }
   next(error)
 }
 
--- models/person.js
+++ models/person.js
@@ -8,8 +8,16 @@ mongoose
   .catch(error => console.log('error connecting to MongoDB', error.message))
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String
+  name: {
+    type: String,
+    minLength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    minLength: 8,
+    required: true
+  }
 })
 
 personSchema.set('toJSON', {
